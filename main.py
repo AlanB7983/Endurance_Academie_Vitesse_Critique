01e96be785a2f341cfe4c927824a6c362dc276cc
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-03-11 13:37:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -625,7 +625,7 @@ if "show_help" not in st.session_state:
     st.session_state.show_help = False
 
 # Création de colonnes pour aligner les éléments
-selec_num_point_col1, selec_num_point_col2, empty_col3 = st.columns([4, 1, 13])  # Ajuster la largeur pour un bon alignement
+selec_num_point_col1, selec_num_point_col2, empty_col3 = st.columns([4, 1, 12])  # Ajuster la largeur pour un bon alignement
 
 with selec_num_point_col1 :
     # Sélection du nombre de tests
