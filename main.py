30d8c9cfc42f4c4273b059453b6f01bc8622a492
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-03-26 14:25:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -921,7 +921,7 @@ if st.session_state.CS is not None:
     LT2_pace_without_unit = LT2_pace[:4]
     LT1_speed, LT1_percent = estimate_LT1(CS, Durability/100.0)
     # LT1_speed = 0.8*CS
-    st.write("LT1 représente " + str(LT1_percent) + " de la vitesse critique)
+    st.write("LT1 représente " + str(LT1_percent) + " de la vitesse critique")
     LT1_pace = speed_to_pace(LT1_speed)
     LT1_pace_without_unit = LT1_pace[:4]
     CS_pace_without_unit = CS_pace[:4]
