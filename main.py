e76f5003020b8690b6c20416b2adaabf1d6ace1d
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-05-02 19:29:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1013,7 +1013,8 @@ if "session" not in st.session_state:
     st.session_state.session = []
 
 # Choix entre ajout d'un bloc ou d'une répétition
-st.markdown("**Sélectionner le type de bloc à ajouter**")
+st.write("\n")
+st.markdown("**Type de bloc à ajouter**")
 choice = st.radio("", ["Bloc simple", "Répétition"], horizontal = True, label_visibility="collapsed")
 
 with st.form("Ajouter un bloc ou une répétition"):
@@ -1051,13 +1052,14 @@ with st.form("Ajouter un bloc ou une répétition"):
         
         st.write("")
         st.subheader("Bloc 2")
-        st.markdown("<p style='font-size:14px;'><strong>Durée du 2ème bloc</strong></p>", unsafe_allow_html=True)
-        duration_bloc_2_col1, duration_bloc_2_col2 = st.columns(2)
-        with duration_bloc_2_col1 :
-            duration2_min = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
-        with duration_bloc_2_col2 :
-            duration2_sec = st.number_input("secondes :", min_value=0, step=1, value=0)
-        duration2 = duration2_min + duration2_sec
+        #st.markdown("<p style='font-size:14px;'><strong>Durée du 2ème bloc</strong></p>", unsafe_allow_html=True)
+        #duration_bloc_2_col1, duration_bloc_2_col2 = st.columns(2)
+        #with duration_bloc_2_col1 :
+        #    duration2_min = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
+        #with duration_bloc_2_col2 :
+        #    duration2_sec = st.number_input("secondes :", min_value=0, step=1, value=0)
+        #duration2 = duration2_min + duration2_sec
+        duration2 = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
         st.write("\n")
         percent_CS2 = st.slider("**Intensité du 2ème bloc (% de CS) :**", min_value=50, max_value=150, step=5, value=75)
         submitted = st.form_submit_button("Ajouter cette répétition")
