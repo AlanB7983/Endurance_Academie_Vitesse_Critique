3828f08e57a8f56e279f3f8ed6fdef7b3b8e154a
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-05-02 20:39:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1058,7 +1058,7 @@ with st.form("Ajouter un bloc ou une répétition"):
             duration2_min = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
         with duration_bloc_2_col2 :
             duration2_sec = st.number_input("secondes :", min_value=0, step=1, value=1)
-        if duration2_min == 0 and duration2_sec == 0 :
+        if duration2_min == 60 and duration2_sec == 1 :
             duration2 = int(1)
         else :
             duration2 = duration2_min + duration2_sec
