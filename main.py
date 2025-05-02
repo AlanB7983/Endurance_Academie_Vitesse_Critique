a0e28f18d5d63c669984d6a5833423fc1fe01f4b
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-05-02 19:31:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1052,14 +1052,15 @@ with st.form("Ajouter un bloc ou une répétition"):
         
         st.write("")
         st.subheader("Bloc 2")
-        #st.markdown("<p style='font-size:14px;'><strong>Durée du 2ème bloc</strong></p>", unsafe_allow_html=True)
-        #duration_bloc_2_col1, duration_bloc_2_col2 = st.columns(2)
-        #with duration_bloc_2_col1 :
-        #    duration2_min = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
-        #with duration_bloc_2_col2 :
-        #    duration2_sec = st.number_input("secondes :", min_value=0, step=1, value=0)
-        #duration2 = duration2_min + duration2_sec
-        duration2 = st.number_input("minutes :", min_value=1, step=1, value=2) * 60
+        st.markdown("<p style='font-size:14px;'><strong>Durée du 2ème bloc</strong></p>", unsafe_allow_html=True)
+        duration_bloc_2_col1, duration_bloc_2_col2 = st.columns(2)
+        duration2_min = 1
+        with duration_bloc_2_col1 :
+            duration2_min = st.number_input("minutes :", min_value=0, step=1, value=1) * 60
+        with duration_bloc_2_col2 :
+            duration2_sec = st.number_input("secondes :", min_value=0, step=1, value=0)
+        duration2 = duration2_min + duration2_sec
+        #duration2 = st.number_input("minutes :", min_value=1, step=1, value=2) * 60
         st.write("\n")
         percent_CS2 = st.slider("**Intensité du 2ème bloc (% de CS) :**", min_value=50, max_value=150, step=5, value=75)
         submitted = st.form_submit_button("Ajouter cette répétition")
