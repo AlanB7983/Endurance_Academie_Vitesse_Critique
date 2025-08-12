109dc265d65e48f0ebaa2e6f60faa45728205962
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2025-08-12 13:49:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1052,13 +1052,13 @@ else :
     # st.write(race_distances)
     if use_power_data :
         st.write("if")
-        if len(race_distances) > 1 and len(race_distances) > 1 :
+        if len(distances) > 1 and len(distances) > 1 :
             st.write("if level2")
             distances, times, powers, power_law_fig = powerlaw_vitesse_et_puissance_append_points(distances,times,powers,t_short = 300.0,t_long = 1200.0)
             st.plotly_chart(power_law_fig, use_container_width=True)
     else :
-        st.write(race_distances)
-        if race_distances[0] != 1000 :
+        st.write(distances)
+        if distances[0] != 1000 :
             st.write("else level2")
             distances, times, powers, power_law_fig = powerlaw_vitesse_et_puissance_append_points(distances,times,[2.0, 1.0],t_short = 300.0,t_long = 1200.0)
             st.plotly_chart(power_law_fig, use_container_width=True)
@@ -1518,6 +1518,7 @@ if st.session_state.session:
     if st.button("Réinitialiser la séance"):
         st.session_state.session = []
         st.rerun()
+
 
 
 
